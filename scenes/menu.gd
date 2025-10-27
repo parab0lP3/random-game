5b14fe5ce1f753adbdfeeb3f1d911bfbbdaaac40
Kenn <239912112+parab0lP3@users.noreply.github.com>
2025-10-27 08:09:00 +0800

    small changes to the sound toggle

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed() -> void:
-	print("Start!")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_settings_button_pressed() -> void:
--- scenes/sound_toggle.gd
+++ scenes/sound_toggle.gd
@@ -1,11 +1,7 @@
 extends "res://scenes/menu_button.gd"
 
-var is_mute: bool
-
 func _on_toggled(toggled_on: bool) -> void:
-	if !is_mute:
-		AudioServer.set_bus_mute(0,true)
-		is_mute = true
-	else:
+	if toggled_on:
 		AudioServer.set_bus_mute(0,false)
-		is_mute = false
+	else:
+		AudioServer.set_bus_mute(0,true)
